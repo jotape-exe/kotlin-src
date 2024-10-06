f63b9b8997ab25d8d512235baadb447de366a129
jotape-exe <joaoxstone@gmail.com>
2024-10-06 15:43:54 -0300

    bassicos essenciais (typealias e extensions)

---
+++ src/typealias_exemplo/Exemplo.kt
@@ -0,0 +1,23 @@
+package typealias_exemplo
+
+typealias Operacao = (Int, Int) -> Int
+
+fun somar(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun subtrair(a: Int, b: Int): Int {
+    return a - b
+}
+
+fun calcular(a: Int, b: Int, operacao: Operacao): Int {
+    return operacao(a, b)
+}
+
+fun main() {
+    val resultadoSoma = calcular(5, 3, ::somar)
+    val resultadoSubtracao = calcular(5, 3, ::subtrair)
+
+    println("Resultado da soma: $resultadoSoma") // 8
+    println("Resultado da subtração: $resultadoSubtracao") // 2
+}
